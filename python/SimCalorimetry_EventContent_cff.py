2b62866d1dec4e87b16d45406d119621ef53840d
William Tanenbaum <sha1-faefd172b4369b2002e8f0f7d11f304fd9896b5b@cern.ch>
2012-05-22 19:26:23 +0000

    SimTracker/SiPixelDigitizer

--- python/SimCalorimetry_EventContent_cff.py
+++ python/SimCalorimetry_EventContent_cff.py
@@ -5,7 +5,8 @@ SimCalorimetryFEVTDEBUG = cms.PSet(
         'keep *_simEcalPreshowerDigis_*_*', 
         'keep *_simEcalTriggerPrimitiveDigis_*_*', 
         'keep *_simHcalDigis_*_*', 
-        'keep ZDCDataFramesSorted_mix_simHcalUnsuppressedDigis*_*',
+        'keep ZDCDataFramesSorted_simHcalUnsuppressedDigis_*_*',
+        'drop ZDCDataFramesSorted_mix_simHcalUnsuppressedDigis*_*',
         'keep *_simHcalTriggerPrimitiveDigis_*_*')
 )
 SimCalorimetryRAW = cms.PSet(
